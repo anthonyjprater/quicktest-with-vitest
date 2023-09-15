6d13c6b791ca313335a6b208f3c7781ef6dc60c4
ajp1984 <prater.anthony.j@gmail.com>
2023-09-15 17:51:15 +0000

    api mock

---
+++ quicktest-with-vitest/src/components/PostCard.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1></h1>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue"
+import axios from "axios"
+
+const post = ref(null)
+const loading = ref(true)
+const error = ref(null)
+
+const fetchPost = async () => {
+    try {
+        const { data } = await axios.get("https://jsonplaceholder.typicode.com/posts/1")
+        post.value = data
+    }
+    catch {
+        error.value = err.message
+    }
+    finally {
+        loading.value = false
+    }
+}
+
+onMounted(() => {
+    fetchPost()
+})
+</script>
